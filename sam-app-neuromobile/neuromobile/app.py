fc665a3366e787dbe568c121aa621499199a4210
David Sánchez <davidsfernandez7@outlook.es>
2023-03-25 18:24:42 +0100

    Updated neuromobile example

---
+++ sam-app-neuromobile/neuromobile/app.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+
+def get_users(event, context):
+    ## Realizamos la llamada a la API de neuromobile
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "retorno de los usuarios",
+        }),
+    }
+
+def get_events(event, context):
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "retorno de los eventos",
+        }),
+    }
